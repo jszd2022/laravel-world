18112d475ccea31ac7593d74f90d9638bbe4319b
János-Széll Dávid <jszdavid@proton.me>
2026-04-12 12:36:40 +0300

    Created basic file structure

---
+++ src/Facades/World.php
@@ -0,0 +1,3 @@
+<?php
+
+namespace JSzD\World\Facades;
---
+++ src/WorldServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace JSzD\World;
+
+use Illuminate\Support\ServiceProvider;
+
+class WorldServiceProvider extends ServiceProvider {
+    /**
+     * Register the application services.
+     */
+    public function register() {
+    }
+
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot() {
+    }
+}
